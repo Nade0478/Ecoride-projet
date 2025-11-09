f44f0a422cc25e8afd20d8ab1088aaf703f99270
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-11-09 05:14:10 +0100

    Modification des divers pages front et de leurs dossiers rattachés

---
+++ ecorider-front/src/components/Admin/CreateCompte.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CompteForm from './CompteForm';
+import AdminLayout from '../Layout/AdminLayout';
+
+const CreateCompte = () => {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'Arial, sans-sherif' }}>
+            <h1>Mon Compte</h1>
+                <p>Gérez les informations de votre compte administrateur ici.</p>
+                <button style={{ marginBottom: '20px', padding: '10px 15px', backgroundColor: '#604B62', color: '#fff', border: 'none', borderRadius: '5px' }}>
+                Modifier le Compte
+                </button>
+        </div>
+    );
+};
+
+export default CreateCompte;
